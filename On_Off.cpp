f5bcda5f14c519e27f52490d5d27b4a4ca204634
KiHwan Kim <kihwan@kookmin.ac.kr>
2018-06-20 14:07:28 +0900

    Update Code for state display

--- On_Off.cpp
+++ On_Off.cpp
@@ -1,13 +1,14 @@
 #include "On_Off.h"
 
-
 void On_Off::Turn_On()
 {
 	this->state = false;
+	std::cout<<"System turns On\n";
 }
 
 void On_Off::Turn_Off()
 {
 	this->state = true;
+	std::cout<<"System turns Off\n";
 }
 
