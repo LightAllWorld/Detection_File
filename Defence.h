aa6225bdf929a0e1b45db1586746283e6315197e
KiHwan Kim <kihwan@kookmin.ac.kr>
2018-06-23 00:14:00 +0900

    Change function name ( HighModel -> Advenced Model ) and append comment to header file

--- Defence.h
+++ Defence.h
@@ -1,3 +1,26 @@
+/************************************************************************************
+* File Name : Defence.h																*
+* Description : This file manages list about exception files.						*
+* Functions :																		*
+*	1. Set_Directory																*
+*		- This is to call a setting directory method from private section.			*
+*	2. Set_Option																	*
+*		- This is to call a setting option method from private section.				*
+*	3. SimpleMode																	*
+*		- This is to set model as a simple detection model.		 					*
+*	4. Advenced_Mode																*
+*		- This is to set model as a advenced detection model.						*	
+*	5. Check_Exception_List 														*
+*		- This works checking the exception list.									*
+*	6. Make_Report																	*
+*		- This makes a report about result of detection.							*
+*	7. Set_Op_Info																	*
+*		- This is to set options.													*
+*	8. Set_Dir_Info																	*
+*		- This is to set directory.													*
+*	9 Preprocessing																	*
+*		- This extracts some features that uses to detection model from input file.	* 
+************************************************************************************/
 #pragma once
 #include <string>
 #include <list>
@@ -8,7 +31,7 @@ public:
 	void Set_Directiory(std::string dir);
 	void Set_Option(std::string option);
 	void SimpleMode();
-	void HighMode();
+	void Advenced_Mode();
 	void Check_Exception_list();
 	void Make_Report();
 	
